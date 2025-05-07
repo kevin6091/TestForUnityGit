e0cb1ebc0df72bfff36587c46841e01a30f2004e
HyeongJunYeom <139520144+HyeongJunYeom@users.noreply.github.com>
2025-05-07 23:16:37 +0900

    testMove
    123

---
+++ Assets/Scenes/Move.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(Time.deltaTime * 3f, 0f, 0f));
+    }
+}
