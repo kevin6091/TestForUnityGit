5ab875adf16f277b6705b40c9ecc35a5092509cd
kevin6091 <145881767+kevin6091@users.noreply.github.com>
2025-05-09 00:28:10 +0900

    ComponentsHelper
    쌈뽕

---
+++ Assets/Scripts/ComponentsHelper.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ComponentsHelper
+{
+    public static bool LoadComponents<T>(GameObject gameObject, ref Dictionary<string, Component> components) where T : Component
+    {
+        T component = gameObject.GetComponent<T>();
+        if(component) 
+        {
+            components.Add(component.name, component);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static T Get<T>(this Dictionary<string, Component> components) where T : Component
+    {
+        string key = typeof(T).Name;
+        if (components.TryGetValue(key, out Component comp))
+            return comp as T;
+
+        Debug.LogError($"Can't found {key}");
+        return null;
+    }
+}
