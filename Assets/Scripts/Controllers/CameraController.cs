73fe6df98098c719caa236aaa489e390d00835d5
HyeongJunYeom <139520144+HyeongJunYeom@users.noreply.github.com>
2025-05-14 18:51:08 +0900

    2차 머지 수정
    123

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -13,6 +13,9 @@ public class CameraController : MonoBehaviour
     float _fov;
     void Start()
     {
+        Managers.Input.KeyAction -= OnKeyboard;
+        Managers.Input.KeyAction += OnKeyboard;
+
         _camera = GetComponent<Camera>();
         if (null == _camera)
         {
@@ -21,6 +24,35 @@ public class CameraController : MonoBehaviour
     }
 
     void Update()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+        if(_mode == Define.CameraMode.QuarterView)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(_player.transform.position, _delta, out hit, _delta.magnitude, LayerMask.GetMask("Block")))
+            {
+                float dist = (hit.point - _player.transform.position).magnitude * 0.8f;
+                transform.position = _player.transform.position + _delta.normalized * dist;
+                transform.LookAt(_player.transform);
+            }
+            else
+            {
+                transform.position = _player.transform.position + _delta;
+                transform.LookAt(_player.transform);
+            }
+        }        
+    }
+
+    public void SetQuaterView(Vector3 delta)
+    {
+
+    }
+
+    void OnKeyboard()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
@@ -43,30 +75,6 @@ public class CameraController : MonoBehaviour
 
             _coInterpolateFov = StartCoroutine(Co_InterpolateFov(30f, 3f));
         }
-    }
-
-    private void LateUpdate()
-    {
-        if(_mode == Define.CameraMode.QuarterView)
-        {
-            RaycastHit hit;
-            if (Physics.Raycast(_player.transform.position, _delta, out hit, _delta.magnitude, LayerMask.GetMask("Wall")))
-            {
-                float dist = (hit.point - _player.transform.position).magnitude * 0.8f;
-                transform.position = _player.transform.position + _delta.normalized * dist;
-                transform.LookAt(_player.transform);
-            }
-            else
-            {
-                transform.position = _player.transform.position + _delta;
-                transform.LookAt(_player.transform);
-            }
-        }        
-    }
-
-    public void SetQuaterView(Vector3 delta)
-    {
-
     }
 
     IEnumerator Co_InterpolateFov(float targetFov, float time)
