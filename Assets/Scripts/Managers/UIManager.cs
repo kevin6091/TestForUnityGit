bf191667e13977cf5ec346196bfdddd08a131932
HyeongJunYeom <139520144+HyeongJunYeom@users.noreply.github.com>
2025-05-14 18:44:53 +0900

    머지 후 1차 수정
    123

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -95,4 +95,10 @@ public class UIManager
         while (_popupStack.Count > 0)
             ClosePopupUI();
     }
+
+    public void Clear()
+    {
+        CloseAllPopupUI();
+        _sceneUI = null;
+    }
 }
--- Assets/Scripts/Utills/Define.cs
+++ Assets/Scripts/Utills/Define.cs
@@ -1,9 +1,32 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Define
 {
+    public enum Layer
+    {
+        Wall = 8,
+        Floor,
+        Block,
+        Monster,
+    }
+
+    public enum Scene
+    {
+        Unknown,    //  Default
+        Login,      //  Main
+        Lobby,      
+        Game,
+    }
+
+    public enum Sound
+    {
+        Bgm,
+        Effect,
+        MaxCount,
+    }
+
     public enum UIEvent
     {
         Click,
@@ -13,11 +36,15 @@ public class Define
     public enum MouseEvent
     {
         Press,
+        PointerDown,
+        PointerUp,
         Click,
     }
 
     public enum CameraMode
     {
         QuarterView,
+        TopView,
+        Free,
     }
 }
