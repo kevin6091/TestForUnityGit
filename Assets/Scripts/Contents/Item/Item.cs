928573435585f516c8c00f505553e556e57a7542
HyeongJunYeom <139520144+HyeongJunYeom@users.noreply.github.com>
2025-05-22 11:49:38 +0900

    Item 컴포넌트추가

---
+++ Assets/Scripts/Contents/Item/Item.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    //  [SerializeField]
+    //  public Define.ItemType itemType { get; private set; } = Define.ItemType.END;
+}
